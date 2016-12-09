79969e9c250b5c3323026f245123b60cc9d2b859
Nattawat <ha.nattawat_st@tni.ac.th>
2016-12-09 03:42:34 +0000

    First commit

---
+++ hello-c-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) {
+   int i;
+   srand(time(0));
+   for(i=0;i<100;i++) printf(" %d",rand()%100+1);
+   putchar('\n');
+   return 0;
+}
